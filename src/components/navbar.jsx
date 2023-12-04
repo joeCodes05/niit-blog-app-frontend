8fe4d10422a4182734448cd9d73f713697ecf824
joeCodes05 <nathancodes05@gmail.com>
2023-12-04 15:15:54 +0100

    worked on navbar UI for both large and small screen sizes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import { Dialog, Popover } from '@headlessui/react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { HiBars3 } from 'react-icons/hi2'
-import { IoCloseOutline } from "react-icons/io5";
-import Logo from '../assets/images/3wz4q4-LogoMakr.png'
+import Logo from '../assets/images/28aOt4-LogoMakr.png'
 import Button from '../utils/buttons';
 import ProfileButton from '../utils/profileBtn';
+import { GoBell } from "react-icons/go";
 
-export default function Navbar () {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
+export default function Navbar() {
   return (
     <header className="bg-dark border-b border-b-gray-600">
       <nav className="mx-auto flex max-w-8xl items-center justify-between p-3 lg:px-8" aria-label="Global">
@@ -19,78 +15,16 @@ export default function Navbar () {
             <img className="h-10 w-auto" src={Logo} alt="swoosh logo" />
           </Link>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <HiBars3 className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div className="hidden lg:flex gap-3 lg:justify-end">
-          <Button 
+        <div className="flex gap-3 lg:justify-end">
+          <Button
             text={'New post'}
           />
+          <button className='ring-1 ring-gray-700 bg-secondary py-1 px-2.5 text-white rounded-md'>
+            <GoBell />
+          </button>
           <ProfileButton />
         </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <IoCloseOutline className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Features
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Marketplace
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Company
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
     </header>
   )
 }
--- src/utils/profileBtn.jsx
+++ src/utils/profileBtn.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const ProfileButton = () => {
   return (
     <>
-      <button className="flex gap-2 items-center bg-secondary p-1 pr-2 rounded-md">
+      <button className="flex gap-2 ring-0 lg:ring-1 ring-gray-700 items-center bg-secondary p-0 lg:p-1 lg:pr-2 rounded-md">
         <div className='bg-primary text-dark py-1 px-1.5 rounded-md'>
           NJ
         </div>
-        <div className='text-white text-sm'>
+        <div className='text-white text-sm hidden lg:block'>
           Nathaniel
         </div>
       </button>
