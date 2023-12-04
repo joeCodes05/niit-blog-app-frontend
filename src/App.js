81b4fc503d41339be10f5b785e20b326a70bfec8
joeCodes05 <nathancodes05@gmail.com>
2023-12-04 15:39:18 +0100

    code update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Navbar from "./components/navbar";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Post from "./pages/blog-single";
+import Profile from "./pages/profile";
+import CreatePost from "./pages/create";
 
 const App = () => {
   return (
@@ -11,6 +16,11 @@ const App = () => {
           <Navbar />
           <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/post/:id" element={<Post />} />
+            <Route exact path="/profile/:id" element={<Profile />} />
+            <Route exact path="/post/create" element={<CreatePost />} />
           </Routes>
         </Router>
       </main>
