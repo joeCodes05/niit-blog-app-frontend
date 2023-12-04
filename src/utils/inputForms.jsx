48ae9a7943bf62e0b2e170ac064f79f1092514d1
joeCodes05 <nathancodes05@gmail.com>
2023-12-04 22:02:28 +0100

    added veiw passwrd feature for input with password as it's type

--- src/utils/inputForms.jsx
+++ src/utils/inputForms.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoEyeOutline, IoEyeOffOutline } from 'react-icons/io5'
 
 const FormControl = ({ placeholder, value, onChange, type }) => {
+  const [seePassword, setSeePassword] = useState(false);
+  const [password, setPassword] = useState('');
+
   return (
     <>
-      <input type={type} className='p-2 outline-none rounded-md bg-secondary ring-1 w-full box-border ring-gray-700 text-white' placeholder={placeholder} value={value} onChange={onChange} required />
+      {type === 'password' ? <div className='outline-none rounded-md overflow-hidden bg-secondary ring-1 ring-gray-700 flex gap-2 items-center'>
+        <input type={seePassword ? 'text' : 'password'} className='p-2 outline-none text-white bg-transparent box-border w-full' placeholder={placeholder} value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <div className='text-gray-500 cursor-pointer px-2' onClick={() => setSeePassword(!seePassword)}>
+          {seePassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
+        </div>
+      </div> : <input type={type} className='p-2 outline-none rounded-md bg-secondary ring-1 w-full box-border ring-gray-700 text-white' placeholder={placeholder} value={value} onChange={onChange} required />}
     </>
   )
 }
