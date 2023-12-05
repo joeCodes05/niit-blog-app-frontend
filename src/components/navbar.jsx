b89e1226504051c6cad59fa1038442322c68195d
joeCodes05 <nathancodes05@gmail.com>
2023-12-05 10:25:09 +0100

    code update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Logo from '../assets/images/28aOt4-LogoMakr.png'
-import Button from '../utils/buttons';
+import {Button} from '../utils/buttons';
 import ProfileButton from '../utils/profileBtn';
 import { GoBell } from "react-icons/go";
 
 export default function Navbar() {
   const location = useLocation();
+  const navigtor = useNavigate();
 
   return (
     <>
-      {location.pathname === '/login' || location.pathname === '/signup' ? null : <header className="bg-dark border-b border-b-gray-600 fixed top-0 left-0 right-0 z-20">
+      {location.pathname === '/login' || location.pathname === '/signup' ? null : <header className="bg-dark border-b border-b-gray-600 fixed top-0 left-0 right-0 z-10">
         <nav className="mx-auto flex max-w-7xl items-center justify-between py-3 lg:px-0 px-3" aria-label="Global">
           <div className="flex lg:flex-1">
             <Link to={'/'} className="-m-1.5 p-1.5">
@@ -20,9 +21,10 @@ export default function Navbar() {
           </div>
           <div className="flex gap-3 lg:justify-end">
             <Button
+              onclick={() => navigtor('/post/create')}
               text={'New post'}
             />
-            <button className='ring-1 ring-gray-700 bg-secondary py-1 px-2.5 text-white rounded-md'>
+            <button className='ring-1 ring-gray-700 bg-secondary outline-none py-1 px-2.5 text-white rounded-md'>
               <GoBell />
             </button>
             <ProfileButton />
