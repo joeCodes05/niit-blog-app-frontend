917fdea55e2b89627f253023db443119dc13547e
joeCodes05 <nathancodes05@gmail.com>
2023-12-06 08:38:26 +0100

    code update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DropdownProvider } from "./context/dropdownContext";
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:8080/api'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
