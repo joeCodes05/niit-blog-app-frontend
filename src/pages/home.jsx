55723c7dc461a533c89b8369ce5b51bf667c8f69
joeCodes05 <nathancodes05@gmail.com>
2023-12-09 21:29:14 +0100

    code update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import CategoryModal from '../utils/categoryFilterModal'
 import PostCard from '../components/postCard'
 
 const Home = () => {
   return (
     <>
-      <section className='max-w-7xl min-h-screen h-full mx-auto lg:px-0 px-3 mt-24 pb-5'>
+      <section className='max-w-7xl min-h-screen h-full mx-auto lg:px-0 md:px-3 mt-24 pb-5'>
         <CategoryModal />
 
         <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:mx-0 mx-5 grid-cols-1 gap-4">
